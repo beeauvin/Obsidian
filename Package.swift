92690e154937a27fb6602ebfec74e5de52db060f
Bee <79487947+beeauvin@users.noreply.github.com>
2025-09-19 23:07:02 -0700

    deps(swift): upgrade to 6.2 (#34)
    Swift 6.2 has quite a few improvements that Obsidian would benefit greatly from. This will be a precursor to the v0.4.0 set of changes.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// swift-tools-version: 6.1
+// swift-tools-version: 6.2
 
 import PackageDescription
 
